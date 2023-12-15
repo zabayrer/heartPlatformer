2320a42052bb53bb471e1ffe8e7b1d5ed5af9240
zabayrer <zabayrer@s.sfusd.edu>
2023-12-15 14:10:22 -0800

    finished video 1
    added player, test area, player script, acceleration, friction, etc.
    made background black

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,27 +2,68 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
+const ACCELERATION = 600.0
 const JUMP_VELOCITY = -300.0
+const FRICTION = 600.0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
+	apply_gravity(delta)
+
+	# Handle Jump.
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY/2:
+			velocity.y = JUMP_VELOCITY/2
+
+	# Get the input input_axis and handle the movement/deceleration.
+	# As good practice, you shouldnt replace UI actions with custom gameplay actions.
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	
+	handle_acceleration(input_axis, delta)
+	
+	apply_friction(input_axis, delta)
+	
+	update_animations(input_axis)
+	
+	move_and_slide()
+
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you shouldnt replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
 	else:
-		velocity.x = 0
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY/2:
+			velocity.y = JUMP_VELOCITY/2
 
-	move_and_slide()
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
+
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = input_axis < 0
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
