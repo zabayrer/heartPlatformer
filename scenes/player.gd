49d699d95b60cc70e52472ef9dd2fe65a83fde4b
zabayrer <zabayrer@s.sfusd.edu>
2024-02-05 14:10:43 -0800

    finished video 4
    basically added nothing, just rearanged things

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,6 @@
 extends CharacterBody2D
 
-
-@export var SPEED = 150.0
-@export var ACCELERATION = 600.0
-@export var JUMP_VELOCITY = -300.0
-@export var FRICTION = 600.0
+@export var movement_data : PlayerMovementData
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_time_timer = $CoyoteTimeTimer
@@ -27,6 +23,7 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	
 	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
 	
 	update_animations(input_axis)
 	
@@ -36,30 +33,33 @@ func _physics_process(delta):
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_time_timer.start()
-		
-
+	
+	
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * movement_data.gravity_scale * delta
 
 
 func handle_jump():
 	if is_on_floor() or coyote_time_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = JUMP_VELOCITY
+			velocity.y = movement_data.jump_velocity
 	if not is_on_floor():
-		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY/2:
-			velocity.y = JUMP_VELOCITY/2
+		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity/2:
+			velocity.y = movement_data.jump_velocity/2
 
 
 func apply_friction(input_axis, delta):
-	if input_axis == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
+	
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 
 func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
 
 
 func update_animations(input_axis):
--- scenes/player_movement_data.gd
+++ scenes/player_movement_data.gd
@@ -2,6 +2,8 @@ class_name PlayerMovementData
 extends Resource
 
 @export var speed = 150.0
-@export var accesleration = 600.0
+@export var acceleration = 600.0
 @export var jump_velocity = -300.0
 @export var friction = 600.0
+@export var gravity_scale = 1.0
+@export var air_resistance = 200.0
