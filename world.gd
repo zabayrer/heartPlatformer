3ec479ec17b94f8d6739b2c9a7869d01a5a41610
zabayrer <zabayrer@s.sfusd.edu>
2024-02-22 13:24:47 -0800

    honestly i have no idea
    something with a tile map; I don't remember if we finished the thing

--- world.gd
+++ world.gd
@@ -1,12 +1,9 @@
 extends Node2D
 
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var level_complete = $Node/LevelComplete
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_complete.connect(show_level_complete)
 	
 func show_level_complete():
